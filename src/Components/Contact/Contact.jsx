c695f62a6158f2d1e0209f2df05b1f4953561294
samiryakhlef <samir.yakhlef30@gmail.com>
2022-11-16 15:21:32 +0100

    mise en place de la navigation et dur router

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Realisation.css'
+
+export default function Contact() {
+    return (
+        <>
+            <h2>Me Contacter</h2>
+            <Link
+                to="/contact">
+            </Link>
+        </>
+    )
+}
---
+++ src/Components/Realisation/Realisation.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Realisation.css'
+import { Link } from 'react-router-dom'
+
+export default function Realisation() {
+    return (
+        <>
+        <h2>Mes Réalisations</h2>
+            <Link
+                to="/realisations">
+            </Link>
+
+        </>
+
+    )
+}
