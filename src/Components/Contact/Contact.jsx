efff2d8b98a2dd76bef93b6ac5ab4a778f2294ef
samiryakhlef <samir.yakhlef30@gmail.com>
2022-11-17 09:46:35 +0100

    récupération de la valeurs des inputs

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Contact.css'
 
 export default function Contact() {
+
+    //state pour récupérer l'input email
+    const [stateInputEmail, setStateInputEmail] = useState();
+
+    //state pour récupérer l'input name
+    const [stateInputName, setStateInputName] = useState();
+
+    //state pour récupérer l'input texte
+    const [stateInputText, setStateInputText] = useState();
+
+    //fonction getInput pour récupérer l'email
+    const getInputEmail = e => {
+        setStateInputEmail(e);
+        console.log(e);
+    }
+    //fonction getInput pour récupérer le name
+    const getInputName = e => {
+        setStateInputName(e);
+        console.log(e);
+    }
+    //fonction getInput pour récupérer le texte
+    const getInputText = e => {
+        setStateInputText(e);
+        console.log(e);
+    }
+
     return (
         <>
             <Link to="/contact"></Link>
@@ -10,11 +36,45 @@ export default function Contact() {
             <form id="msform">
                 <fieldset>
                     <h2 className="title"> Me contacter</h2>
-                    <input type="text" name="email" placeholder="Email" />
-                    <input type="text" name="name" placeholder="Nom" />
-                    <textarea name="" id="" cols="20" rows="5" placeholder='Taper votre message...'></textarea>
+                    <input
+                        type="text"
+                        name="email"
+                        placeholder="Email"
+
+                        // je récupère le state de l'input Email 
+                        value={stateInputEmail}
+
+                        // je récupère la fonction  pour récupérer la valeur de l'input
+                        onInput={e => getInputEmail(e.target.value)} />
+
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Nom"
+
+                        // je récupère le state de l'input Email 
+                        value={stateInputName}
+
+                        // je récupère la fonction  pour récupérer la valeur de l'input
+                        onInput={e => getInputName(e.target.value)} />
+
+                    <textarea
+                        name="text"
+                        id="text"
+                        cols="20"
+                        rows="5"
+                        placeholder='Taper votre message...'
+
+                        // je récupère le state de l'input Email 
+                        value={stateInputText}
+
+                        // je récupère la fonction  pour récupérer la valeur de l'input
+                        onInput={e => getInputText(e.target.value)}></textarea>
                 </fieldset>
-                <input type="submit" name="submit" class="submit action-button" value="Envoyer" />
+
+                <button
+                    className="submit action-button">
+                    Envoyer</button>
             </form>
 
 
